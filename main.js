7f8b8d8fbfd9c8f8557a60b2aaa8f0e0ef26247b
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-10-21 23:36:40 -0500

    added basic dialogue function

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
+let dialogueBox = document.getElementById("dialogue");
+let replyBox = document.getElementById("replies");
+let currentLine = document.getElementById("currentLine");
+
 class Dialogue {
     constructor(lines, replies) {
-        lines,
-        replies
+        this.lines = lines,
+        this.replies = replies
+        this.index = 1
+    }
+
+    checkForReplies() {
+        if (this.replies.has(this.index.toString())) {
+            replyBox.innerHTML = ""
+            this.replies.get(this.index.toString()).forEach((reply) => {
+                let response = document.createElement("div")
+                response.className = "reply"
+                response.innerHTML = reply.displayText
+                replyBox.appendChild(response)
+            })
+        }
     }
 
     startDialogue() {
-
+        this.index = 0
+        currentLine.innerText = lines[0]
+        dialogueBox.style.display = "flex"
     }
+}
+
+
+const test2 = {
+    1: [["display this", "line after 1", "line after 2"], ["display this 2nd", "after you display 2", "after 2"]]
 }
 
 
 let generateDialogueReplies = (replies) => {
     const Returnreplies = new Map()
-    replies.forEach((reply) => {
-        Returnreplies.set(reply[0], {
-            displayText: reply[1],
-            linesAfter: reply.slice(2)
-        })
+    Object.keys(replies).forEach((reply) => {
+        Returnreplies.set(reply, turnToObject(replies[reply]))
     })
 
     return Returnreplies
 }
 
-const D1 = new Dialogue(["your mom", "your dad"], generateDialogueReplies[[1, "display this!", "this is line 1", "this is line 2"]])
+let turnToObject = (array) => {
+    let ReturnObject = [];
+    array.forEach((item) => ReturnObject.push({displayText: item[0], linesAfter: item.slice(1)}))
+    return ReturnObject
+}
+
+console.log(generateDialogueReplies(test2))
+
+const D1 = new Dialogue(["your mom", "your dad"], generateDialogueReplies(test2))
+
+D1.checkForReplies()
