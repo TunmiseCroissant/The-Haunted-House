2a816845f6396b645cacab8d9adc613e33588ca6
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-10-23 23:37:06 -0500

    fixed and completed dialogue system

--- main.js
+++ main.js
@@ -7,41 +7,69 @@ class Dialogue {
         this.lines = lines,
         this.replies = replies
         this.index = 0
+        this.ResponeLines = []
+        this.ReponseIndex = 0
+        this.usingResponseLines = false
+        this.running = false
     }
-
-    checkForReplies() {
-        if (this.replies.has(this.index.toString())) {
-            replyBox.innerHTML = ""
-            this.replies.get(this.index.toString()).forEach((reply) => {
-                let response = document.createElement("div")
-                response.className = "reply"
-                response.innerHTML = reply.displayText
-                replyBox.appendChild(response)
-            })
-
-            return true
-        }
-
-        return false
-    }
-
 
     startDialogue() {
         this.index = 0
-        currentLine.innerText = this.lines[0]
-        this.checkForReplies()
+        currentLine.innerText = this.lines[this.index]
         dialogueBox.style.display = "flex"
-        dialogueBox.addEventListener("click", () => {
-            if ((this.lines.length - 1 === this.index) && !this.checkForReplies) {
-                dialogueBox.style.display = "none"
-            }
-            if (!this.checkForReplies()) {
-                this.index++
-                currentLine.innerText = this.lines[this.index]
-                this.checkForReplies()
+        this.usingResponseLines = false
+        this.checkLinesToUse()
+        this.running = true
+        currentLine.addEventListener("click", () => {
+            if (!this.usingResponseLines) {
+                    if (this.index === this.lines.length - 1) {
+                        dialogueBox.style.display = "none"
+                    } else {
+                        this.index++
+                        currentLine.innerText = this.lines[this.index]
+                    }
+                    this.checkLinesToUse()
+
+            } else if (this.usingResponseLines) {
+                if (this.ReponseIndex === this.ResponeLines.length - 1) {
+                        if (this.index === this.lines.length - 1) {
+                            dialogueBox.style.display = "none"
+                        } else {
+                            this.usingResponseLines = false
+                            this.index++
+                            currentLine.innerText = this.lines[this.index]
+                            this.checkLinesToUse()
+                        }
+                    } else if (this.running) {
+                        this.ReponseIndex++
+                        currentLine.innerText = this.ResponeLines[this.ReponseIndex]
+                    }
             }
         })
     }
+
+    checkLinesToUse() {
+        this.StringIndex = this.index.toString()
+        if (this.replies.has(this.StringIndex)) {
+            this.usingResponseLines = true
+            this.running = false
+            replyBox.innerHTML = ""
+            this.replies.get(this.StringIndex).forEach((reply) => {
+                this.response = document.createElement("div")
+                this.response.className = "reply"
+                this.response.innerHTML = reply.displayText
+                replyBox.appendChild(this.response)
+                this.response.addEventListener("click", () => {
+                    this.running = true
+                    this.ResponeLines = reply.linesAfter
+                    this.ReponseIndex = 0
+                    currentLine.innerText = this.ResponeLines[0]
+                    replyBox.innerHTML = ""
+                })
+            })
+        }
+    }
+
 }
 
 
@@ -65,8 +93,7 @@ let turnToObject = (array) => {
     return ReturnObject
 }
 
-console.log(generateDialogueReplies(test2))
 
-const D1 = new Dialogue(["your mom", "your dad"], generateDialogueReplies(test2))
+const D1 = new Dialogue(["your mom", "your dad", "your cat"], generateDialogueReplies(test2))
 
 D1.startDialogue()
