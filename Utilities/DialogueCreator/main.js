1df60850a88baaef4dc8058eabf5f3ceb68fdda0
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-10-27 20:12:31 -0500

    First Dialogue line can add and then normal editor shows

--- Utilities/DialogueCreator/main.js
+++ Utilities/DialogueCreator/main.js
@@ -10,6 +10,7 @@ class Dialogue {
     constructor (name, speakers) {
         this.name = name
         this.speakers = speakers
+        this.lines = []
     }
 
 }
@@ -19,7 +20,7 @@ const addDialogue = () => {
     document.getElementById("cancelButton").addEventListener("click", () => {
         if (!Object.keys(dialogues).length === 0) popUp.close();
     })
-    document.getElementById("NewDialogue").addEventListener("submit", () => {
+    document.getElementById("NewDialogue").addEventListener("submit", (event) => {
         event.preventDefault();
 
         let NewDialogue = new FormData(document.getElementById("NewDialogue"))
@@ -33,9 +34,14 @@ const addDialogue = () => {
         dialogues[NewDialogue.get("DialogueName")] = new Dialogue(NewDialogue.get("DialogueName"), filteredSpeakers)
         currentDialogue = dialogues[NewDialogue.get("DialogueName")]
         showEditor("empty")
+        setTitle()
         popUp.close()
         refreshSelector()
     })
+}
+
+const setTitle = () => {
+    document.getElementById("DialogueTitle").innerText = currentDialogue.name
 }
 
 const refreshSelector = () => {
@@ -58,6 +64,7 @@ Object.keys(dialogues).length === 0 ? addDialogue() : LoadDialoguesFromStorage()
 
  document.getElementById("SelectDialogue").addEventListener("change", (e) => {
     console.log(e.target.value)
+    setTitle()
  })
 
  const showEditor = (editor = "line") => {
@@ -71,9 +78,30 @@ Object.keys(dialogues).length === 0 ? addDialogue() : LoadDialoguesFromStorage()
         document.getElementById("Empty").style.display = "none"
         document.getElementById("replyEditor").style.display = "none"
         document.getElementById("LineEditor").style.display = "flex"
+        document.getElementById("lineType").style.display = "inline"
+        document.getElementById("lineTypeLabel").style.display = "inline"
     } else if (editor.toLowerCase() === "reply") {
         document.getElementById("Empty").style.display = "none"
         document.getElementById("replyEditor").style.display = "flex"
         document.getElementById("LineEditor").style.display = "none"
+        document.getElementById("lineType").style.display = "inline"
+        document.getElementById("lineTypeLabel").style.display = "inline"
     }
  }
+
+ const refreshDialogueLines = () => {
+    let DialogueHTML = ""
+    currentDialogue.lines.forEach(([speaker, line]) => {
+        DialogueHTML += `<p>${speaker}: ${line}</p>`
+    })
+    document.getElementById("dialogue").innerHTML = DialogueHTML
+ }
+
+ document.getElementById("EmptyForm").addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    let NewLine = new FormData(document.getElementById("EmptyForm"))
+    currentDialogue.lines.push([NewLine.get("speaker"), NewLine.get("line")])
+    refreshDialogueLines()
+    showEditor()
+ })
