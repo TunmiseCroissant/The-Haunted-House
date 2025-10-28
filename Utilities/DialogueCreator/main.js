144a61dabe0300dde928c919394b9a4184a7594f
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-10-27 19:02:10 -0500

    Added beginning dialogue popup

--- Utilities/DialogueCreator/main.js
+++ Utilities/DialogueCreator/main.js
@@ -1,8 +1,15 @@
-const dialogues = JSON.parse(localStorage.getItem("dialogues")) || [];
+const dialogues = JSON.parse(localStorage.getItem("dialogues")) || {};
 const popUp = document.getElementById("popUp");
 
 
 const LoadDialoguesFromStorage = () => {
+
+}
+
+class Dialogue {
+    constructor (speakers) {
+        this.speakers = speakers
+    }
 
 }
 
@@ -11,6 +18,30 @@ const addDialogue = () => {
     document.getElementById("cancelButton").addEventListener("click", () => {
         popUp.close();
     })
+    document.getElementById("NewDialogue").addEventListener("submit", () => {
+        event.preventDefault();
+
+        let NewDialogue = new FormData(document.getElementById("NewDialogue"))
+        let Speakers = NewDialogue.get("Speakers").split(",").map(item => item.trim()).filter(item => item);
+        let filteredSpeakers = []
+        Speakers.forEach(speaker => {
+            if (!filteredSpeakers.includes(speaker)) {
+                filteredSpeakers.push(speaker)
+            }
+        })
+        dialogues[NewDialogue.get("DialogueName")] = new Dialogue(filteredSpeakers)
+        console.log(filteredSpeakers)
+        popUp.close()
+        refreshSelector()
+    })
 }
 
-dialogues.length === 0 ? addDialogue() : LoadDialoguesFromStorage()
+const refreshSelector = () => {
+    let DialogueOptions = ""
+    Object.keys(dialogues).forEach((key) => {
+        DialogueOptions += `<option>${key}</option>`
+    })
+    document.getElementById("SelectDialogue").innerHTML = DialogueOptions
+}
+
+Object.keys(dialogues).length === 0 ? addDialogue() : LoadDialoguesFromStorage()
